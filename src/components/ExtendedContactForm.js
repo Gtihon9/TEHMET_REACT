91414c695765f529de1f5ddbb46d2c45f9b124fb
Gtihon9 <gtihon9@gmail.com>
2023-08-09 17:24:46 +0300

    Projects Page Done

--- src/components/ExtendedContactForm.js
+++ src/components/ExtendedContactForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useRef, useState} from 'react';
 import "./ExtendedContactForm.css"
 import ArrowSVG from "./Arrow";
 const ExtendedContactForm = () => {
@@ -12,6 +12,7 @@ const ExtendedContactForm = () => {
     deadline: '',
     photos: [],
   });
+  const fileInputRef = useRef(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -23,10 +24,27 @@ const ExtendedContactForm = () => {
     setFormData((prevData) => ({ ...prevData, photos: files }));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-  };
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+    const response = await fetch('https://your-api-endpoint.com/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      console.log('Form data submitted successfully');
+    } else {
+      console.error('Form data submission failed');
+    }
+  } catch (error) {
+    console.error('An error occurred while submitting the form:', error);
+  }
+};
 
   return (
       <div className="main-content-container-3">
@@ -59,12 +77,47 @@ const ExtendedContactForm = () => {
                                     <input type="email" name="email" placeholder="Укажите email..." required value={formData.email} onChange={handleChange} />
                                     <input type="tel" name="phone" placeholder="Укажите телефон..." required value={formData.phone} onChange={handleChange} />
                                 </div>
-                                <textarea name="aim" placeholder="Укажите цель работы" required value={formData.aim} onChange={handleChange}></textarea>
-                                <textarea name="address" placeholder="Укажите адрес объекта" required value={formData.address} onChange={handleChange}></textarea>
-                                <input type="text" name="sizes" placeholder="Укажите объем рыбот или его габариты" required value={formData.sizes} onChange={handleChange} />
-                                <input type="date" name="deadline" placeholder="Желаемые сроки выполнения работы" required value={formData.deadline} onChange={handleChange} />
-                                <input style={{ all: "initial"}} type="file" name="photos"  multiple onChange={handlePhotoChange} />
-                                <button type="submit">Submit</button>
+                                <div className="form-columns">
+                                    <textarea name="aim" placeholder="Укажите цель работы" required value={formData.aim} onChange={handleChange}></textarea>
+                                    <textarea name="address" placeholder="Укажите адрес объекта" required value={formData.address} onChange={handleChange}></textarea>
+                                    <input type="text" name="sizes" placeholder="Укажите объем работ или его габариты" required value={formData.sizes} onChange={handleChange} />
+                                    <input type="date" name="deadline" placeholder="Желаемые сроки выполнения работы" required value={formData.deadline} onChange={handleChange} />
+                                    <div className="file-input-container">
+                                        <label htmlFor="fileInput" className="file-input-label">
+                                            {formData.photos.length > 0 ? 'Изменить фото' : 'Прикрепить фото'}
+                                            <input
+                                                style={{display: "none"}}
+                                                id="fileInput"
+                                                className="file-input"
+                                                type="file"
+                                                name="photos"
+                                                accept="image/*"
+                                                multiple
+                                                onChange={handlePhotoChange}
+                                                ref={fileInputRef}
+                                        />
+                                        </label>
+                                          {formData.photos.length > 0 && (
+                                            <div className="photo-preview">
+                                              {formData.photos.map((photo, index) => (
+                                                <img
+                                                  key={index}
+                                                  src={URL.createObjectURL(photo)}
+                                                  alt={`Preview ${index}`}
+                                                />
+                                              ))}
+                                            </div>
+                                          )}
+                                    </div>
+                                </div>
+                                <div className="submit-container">
+                                    <button type="submit">Отправить</button>
+                                    <p>
+                                         Нажимая на кнопку "Отправить", я подтверждаю, что <br/>
+                                         ознакомился с <a href="/conf">Политикой конфиденциальностии</a>  даю согласие <br/>
+                                         на обработку всех моих персональных данных
+                                     </p>
+                                </div>
                             </div>
 
                     </form>
