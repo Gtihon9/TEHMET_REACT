669423cc99f1e30ee8130f5ff7a9cf74682568d7
Gtihon9 <gtihon9@gmail.com>
2023-08-11 18:50:26 +0300

    Services pages

--- src/pages/Deals.js
+++ src/pages/Deals.js
@@ -2,13 +2,14 @@ import React from 'react';
 import '../components/reset.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Services from "../components/Services";
 
 function Home() {
   return (
     <div>
         <Header/>
 
-        <div>Deals </div>
+        <Services/>
 
         <Footer/>
     </div>
