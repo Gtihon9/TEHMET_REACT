20de58118266935126e873e4b84a25ee7c790a2d
oleg <olegsmushko@gmail.com>
2023-08-16 16:14:21 +0300

    Rent Dialog Form

---
+++ src/components/Rent/RentDialog.js
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react"
+import "./RentDialog.css"
+import { Button } from "../Button/Button"
+
+export const RentDialog = ({ isOpen, onClose }) => {
+   const [formData, setFormData] = useState({
+      name: "",
+      email: "",
+      phoneNumber: "",
+      rentPeriod: ""
+   })
+
+   const handleSubmit = (e) => {
+      e.preventDefault()
+      console.log(formData);
+   }
+
+   const handleChange = (e) => {
+      const { name, value } = e.target
+      setFormData((prev) => ({ ...prev, [name]: value }))
+   }
+
+   const onKeydown = ({ key }) => {
+      if (key === "Escape") {
+         onClose()
+      }
+   }
+
+   useEffect(() => {
+      document.addEventListener("keydown", onKeydown)
+      return () => document.removeEventListener("keydown", onKeydown)
+   })
+
+   if (!isOpen) return null
+
+   return (
+      <div className="modal" onClick={onClose}>
+         <div className="modal-dialog" onClick={e => e.stopPropagation()}>
+            <span className="modal-close" onClick={onClose}>
+               &times;
+            </span>
+            <div className="modal-content">
+               <div className="modal-content-left">
+                  <h3>Свяжитесь с нами</h3>
+                  <form method="POST" onSubmit={handleSubmit}>
+                     <div className="modal-body">
+                        <input
+                           name="name"
+                           placeholder="Укажите имя..."
+                           type="text"
+                           required
+                           value={formData.name}
+                           onChange={handleChange}
+                        />
+                        <input
+                           name="email"
+                           placeholder="Укажите email..."
+                           type="email"
+                           required
+                           value={formData.email}
+                           onChange={handleChange}
+                        />
+                        <input
+                           name="phoneNumber"
+                           placeholder="Укажите телефон..."
+                           type="tel"
+                           required
+                           value={formData.phoneNumber}
+                           onChange={handleChange}
+                        />
+                        <input
+                           name="rentPeriod"
+                           placeholder="Срок аренды..."
+                           type="date"
+                           required
+                           value={formData.rentPeriod}
+                           onChange={handleChange}
+                        />
+                     </div>
+                     <div className="modal-footer">
+                        <Button type="submit">Отправить</Button>
+                        <p>
+                           Нажимая на кнопку "Отправить", я подтверждаю, что <br />
+                           ознакомился с <a href="/conf">Политикой конфиденциальностии</a> даю согласие{" "}
+                           <br />
+                           на обработку всех моих персональных данных
+                        </p>
+                     </div>
+                  </form>
+               </div>
+               <div className="modal-content-right">
+                  <h3>Контакты</h3>
+                  <div className="modal-content-contacts">
+                     <div className="contact">
+                        <span>Телефон</span>
+                        <p>+7-916-900-42-55</p>
+                     </div>
+                     <div className="contact">
+                        <span>Email</span>
+                        <p>fdv240@gmail.com</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   )
+}
--- src/components/Rent/RentItem.js
+++ src/components/Rent/RentItem.js
@@ -1,26 +1,33 @@
 import { Button } from "../Button/Button"
+import { RentDialog } from "./RentDialog"
+import { useDisclosure } from "../../hooks/useDisclosure"
 import "./RentItem.css"
 
 export const RentItem = ({ item }) => {
-	return (
-		<div className="rent-item">
-			<img className="rent-item-image" alt={item.name} src={item.imageUrl} />
-			<div className="rent-item-content">
-				<div className="rent-item-heading">
-					<p>{item.name}</p>
-					<div className="rent-item-price">
-						<p>{item.price}</p>
-						<span> / смена</span>
-					</div>
-				</div>
+	const { isOpen, onClose, onOpen } = useDisclosure()
 
-				<div className="rent-item-settings">
-					{item.settings.map(setting => (
-						<p key={setting + item.name}>{setting}</p>
-					))}
+	return (
+		<>
+			<div className="rent-item">
+				<img className="rent-item-image" alt={item.name} src={item.imageUrl} />
+				<div className="rent-item-content">
+					<div className="rent-item-heading">
+						<p>{item.name}</p>
+						<div className="rent-item-price">
+							<p>{item.price}</p>
+							<span> / смена</span>
+						</div>
+					</div>
+
+					<div className="rent-item-settings">
+						{item.settings.map(setting => (
+							<p key={setting + item.name}>{setting}</p>
+						))}
+					</div>
+					<Button className="rent-item-button" onClick={onOpen}>Арендовать</Button>
 				</div>
-				<Button className="rent-item-button">Арендовать</Button>
 			</div>
-		</div>
+			<RentDialog isOpen={isOpen} onClose={onClose} />
+		</>
 	)
 }
