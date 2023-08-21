2121795804587f4a8527f62477375218288eab54
oleg <olegsmushko@gmail.com>
2023-08-21 15:00:21 +0300

    Responsive Service Item Fix

--- src/components/Services/ServicesItem.js
+++ src/components/Services/ServicesItem.js
@@ -9,12 +9,12 @@ export const ServicesItem = ({ service }) => {
       <div className="services-item">
          <div className="services-item-content">
             <ArrowHeading title={service.title} description={service.description} />
-            <Button className="services-item-content-button">
-               <Link to={service.link}>
+            <Link to={service.link} className="services-item-content-button">
+               <Button >
                   Подробнее
-               </Link>
-               <ShareIcon />
-            </Button>
+                  <ShareIcon />
+               </Button>
+            </Link>
          </div>
          <div className="services-item-image">
             <img alt={service.title} src={service.image} />
