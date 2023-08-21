a855c33b3ab937741a16cac5e44c663ba1cebc9c
oleg <olegsmushko@gmail.com>
2023-08-21 13:20:57 +0300

    Container fix

--- src/components/ExtendedContactForm/ExtendedContactForm.js
+++ src/components/ExtendedContactForm/ExtendedContactForm.js
@@ -42,99 +42,101 @@ const ExtendedContactForm = () => {
 	}
 
 	return (
-		<div className="main-content-container-3 container">
-			<div className="form-main-content">
-				<ArrowHeading
-					title="Получите коммерческое предложение"
-					description="Опишите свой объект для лучшего расчета работы и выгодного решения"
-				/>
-				<div className="content">
-					<p>
-						Подробное описание объекта, для грядущей работе, поможет обеспечить эффективность
-						процесса. Сюда можно включить такие сведения, как адрес объекта, его площадь,
-						количество этажей или уровней, материалы, использованные при строительстве, а
-						также любые уникальные или примечательные особенности. Для большей ясности можно
-						также включить любую сопроводительную документацию или изображения. Предоставление
-						максимально подробной информации поможет свести к минимуму задержки и обеспечить
-						безопасность всех участников проекта.
-					</p>
+		<div className="container">
+			<div className="main-content-container-3">
+				<div className="form-main-content">
+					<ArrowHeading
+						title="Получите коммерческое предложение"
+						description="Опишите свой объект для лучшего расчета работы и выгодного решения"
+					/>
+					<div className="content">
+						<p>
+							Подробное описание объекта, для грядущей работе, поможет обеспечить эффективность
+							процесса. Сюда можно включить такие сведения, как адрес объекта, его площадь,
+							количество этажей или уровней, материалы, использованные при строительстве, а
+							также любые уникальные или примечательные особенности. Для большей ясности можно
+							также включить любую сопроводительную документацию или изображения. Предоставление
+							максимально подробной информации поможет свести к минимуму задержки и обеспечить
+							безопасность всех участников проекта.
+						</p>
 
-					<form
-						className="extended-contact-form"
-						action="/submit"
-						method="POST"
-						onSubmit={handleSubmit}
-					>
-						<div className="form">
-							<div className="three-rows">
-								<input
-									type="text"
-									name="name"
-									placeholder="Укажите имя..."
-									required
-									value={formData.name}
-									onChange={handleChange}
-								/>
-								<input
-									type="email"
-									name="email"
-									placeholder="Укажите email..."
-									required
-									value={formData.email}
-									onChange={handleChange}
-								/>
-								<input
-									type="tel"
-									name="phone"
-									placeholder="Укажите телефон..."
-									required
-									value={formData.phone}
-									onChange={handleChange}
-								/>
+						<form
+							className="extended-contact-form"
+							action="/submit"
+							method="POST"
+							onSubmit={handleSubmit}
+						>
+							<div className="form">
+								<div className="three-rows">
+									<input
+										type="text"
+										name="name"
+										placeholder="Укажите имя..."
+										required
+										value={formData.name}
+										onChange={handleChange}
+									/>
+									<input
+										type="email"
+										name="email"
+										placeholder="Укажите email..."
+										required
+										value={formData.email}
+										onChange={handleChange}
+									/>
+									<input
+										type="tel"
+										name="phone"
+										placeholder="Укажите телефон..."
+										required
+										value={formData.phone}
+										onChange={handleChange}
+									/>
+								</div>
+								<div className="form-columns">
+									<textarea
+										name="aim"
+										placeholder="Укажите цель работы"
+										required
+										value={formData.aim}
+										onChange={handleChange}
+									></textarea>
+									<textarea
+										name="address"
+										placeholder="Укажите адрес объекта"
+										required
+										value={formData.address}
+										onChange={handleChange}
+									></textarea>
+									<input
+										type="text"
+										name="sizes"
+										placeholder="Укажите объем работ или его габариты"
+										required
+										value={formData.sizes}
+										onChange={handleChange}
+									/>
+									<input
+										type="date"
+										name="deadline"
+										placeholder="Желаемые сроки выполнения работы"
+										required
+										value={formData.deadline}
+										onChange={handleChange}
+									/>
+								</div>
+								<div className="submit-container">
+									<Button type="submit">Отправить</Button>
+									<p>
+										Нажимая на кнопку "Отправить", я подтверждаю, что <br />
+										ознакомился с <a href="/conf">Политикой конфиденциальностии</a> даю
+										согласие <br />
+										на обработку всех моих персональных данных
+									</p>
+								</div>
 							</div>
-							<div className="form-columns">
-								<textarea
-									name="aim"
-									placeholder="Укажите цель работы"
-									required
-									value={formData.aim}
-									onChange={handleChange}
-								></textarea>
-								<textarea
-									name="address"
-									placeholder="Укажите адрес объекта"
-									required
-									value={formData.address}
-									onChange={handleChange}
-								></textarea>
-								<input
-									type="text"
-									name="sizes"
-									placeholder="Укажите объем работ или его габариты"
-									required
-									value={formData.sizes}
-									onChange={handleChange}
-								/>
-								<input
-									type="date"
-									name="deadline"
-									placeholder="Желаемые сроки выполнения работы"
-									required
-									value={formData.deadline}
-									onChange={handleChange}
-								/>
-							</div>
-							<div className="submit-container">
-								<Button type="submit">Отправить</Button>
-								<p>
-									Нажимая на кнопку "Отправить", я подтверждаю, что <br />
-									ознакомился с <a href="/conf">Политикой конфиденциальностии</a> даю
-									согласие <br />
-									на обработку всех моих персональных данных
-								</p>
-							</div>
-						</div>
-					</form>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,78 +6,80 @@ import "./Footer.css"
 const Footer = () => {
 	return (
 		<footer className="footer">
-			<div className="footer-content container">
-				<div className="footer-column">
-					<div className="logo">
-						<div className="logo-wrapper">
-							<img className="logo-image" src={Logo} alt="Logo" />
-							<img className="logo-image-text" src={LogoText} alt="Logo Text" />
+			<div className="container">
+				<div className="footer-content">
+					<div className="footer-column">
+						<div className="logo">
+							<div className="logo-wrapper">
+								<img className="logo-image" src={Logo} alt="Logo" />
+								<img className="logo-image-text" src={LogoText} alt="Logo Text" />
+							</div>
+						</div>
+						<div className="address-info">
+							<p>
+								Московская область, городской округ Химки,
+								<br />
+								город Химки, улица Рабочая, д. 2А,
+								<br />
+								кабинет 13.
+							</p>
+							<p>+7-916-900-42-55</p>
+							<p>fdv240@gmail.com</p>
 						</div>
 					</div>
-					<div className="address-info">
-						<p>
-							Московская область, городской округ Химки,
-							<br />
-							город Химки, улица Рабочая, д. 2А,
-							<br />
-							кабинет 13.
-						</p>
-						<p>+7-916-900-42-55</p>
-						<p>fdv240@gmail.com</p>
+					<div className="footer-column">
+						<div className="footer-column-name">Компания</div>
+						<ul>
+							<li>
+								<Link to="/company">О компании</Link>
+							</li>
+							<li>
+								<Link to="/projects">Проекты</Link>
+							</li>
+							<li>
+								<Link to="/services">Услуги</Link>
+							</li>
+							<li>
+								<Link to="/rent">Аренда</Link>
+							</li>
+							<li>
+								<Link to="/news">Новости</Link>
+							</li>
+							<li>
+								<Link to="/jobs">Вакансии</Link>
+							</li>
+							<li>
+								<Link to="/cert">Сертификаты и лицензии</Link>
+							</li>
+						</ul>
 					</div>
-				</div>
-				<div className="footer-column">
-					<div className="footer-column-name">Компания</div>
-					<ul>
-						<li>
-							<Link to="/company">О компании</Link>
-						</li>
-						<li>
-							<Link to="/projects">Проекты</Link>
-						</li>
-						<li>
-							<Link to="/services">Услуги</Link>
-						</li>
-						<li>
-							<Link to="/rent">Аренда</Link>
-						</li>
-						<li>
-							<Link to="/news">Новости</Link>
-						</li>
-						<li>
-							<Link to="/jobs">Вакансии</Link>
-						</li>
-						<li>
-							<Link to="/cert">Сертификаты и лицензии</Link>
-						</li>
-					</ul>
-				</div>
-				<div className="footer-column">
-					<div className="footer-column-name">Направления комании</div>
-					<ul>
-						<li>
-							<Link to="/services/demolition">Снос и демонтаж</Link>
-						</li>
-						<li>
-							<Link to="/services/pit-developing">Разработка и ограждение котлованов</Link>
-						</li>
-						<li>
-							<Link to="/services/renovation">Реновация территории</Link>
-						</li>
-						<li>
-							<Link to="services/recycling">Рециклинг</Link>
-						</li>
-						<li>
-							<Link to="/rent">Аренда</Link>
-						</li>
-					</ul>
-				</div>
-				<div className="footer-column">
-					<div className="footer-column-name">ООО «Техметсервис»</div>
-					<div className="address-info">
-						<p className="address-line-1">ОГРН 1035009566822</p>
-						<p className="address-line-2">ИНН 5047048624</p>
-						<Link to="/conf">Политика конфиденциальности</Link>
+					<div className="footer-column">
+						<div className="footer-column-name">Направления комании</div>
+						<ul>
+							<li>
+								<Link to="/services/demolition">Снос и демонтаж</Link>
+							</li>
+							<li>
+								<Link to="/services/pit-developing">Разработка и ограждение котлованов</Link>
+							</li>
+							<li>
+								<Link to="/services/renovation">Реновация территории</Link>
+							</li>
+							<li>
+								<Link to="services/recycling">Рециклинг</Link>
+							</li>
+							<li>
+								<Link to="/rent">Аренда</Link>
+							</li>
+						</ul>
+					</div>
+					<div className="footer-column">
+						<div className="footer-column-name">ООО «Техметсервис»</div>
+						<div className="address-info">
+							<p className="address-line-1">ОГРН 1035009566822</p>
+							<p className="address-line-2">ИНН 5047048624</p>
+							<Link to="/conf">Политика конфиденциальности</Link>
+						</div>
 					</div>
 				</div>
 			</div>
