95cad70a3e871f581dc077587b06708d93bd9a6a
oleg <olegsmushko@gmail.com>
2023-08-23 13:10:44 +0300

    ConfInfo component done

---
+++ src/components/ConfInfo/ConfInfo.js
@@ -0,0 +1,12 @@
+import "./ConfInfo.css"
+import { Link } from "react-router-dom"
+
+export const ConfInfo = () => {
+	return (
+		<span className="conf-info">
+			Нажимая на кнопку "Отправить", я подтверждаю, что ознакомился с{" "}
+			<Link to="/conf">Политикой конфиденциальности </Link>и даю согласие на обработку всех моих
+			персональных данных
+		</span>
+	)
+}
