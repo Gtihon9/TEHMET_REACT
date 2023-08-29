bdd2cc3fdd476d2772acb8d9d9079b6554bb48dc
oleg <olegsmushko@gmail.com>
2023-08-29 14:46:16 +0300

    Lazy image component

---
+++ src/components/LazyImage/LazyImage.js
@@ -0,0 +1,22 @@
+import "./LazyImage.css"
+import { useEffect, useState } from "react"
+
+export const LazyImage = ({ placeholderSrc, alt, src, className, props }) => {
+	const [imageSrc, setImageSrc] = useState(placeholderSrc)
+
+	useEffect(() => {
+		const img = new Image()
+		img.src = src
+		img.onload = () => {
+			setImageSrc(src)
+		}
+	}, [src])
+
+	const cn = `lazy-image ${placeholderSrc === imageSrc ? "loading" : "loaded"} ${
+		className ? className : ""
+	}`
+
+	return (
+		<img alt={alt || ""} src={imageSrc} width="100%" height="100%" className={cn} {...props} />
+	)
+}
