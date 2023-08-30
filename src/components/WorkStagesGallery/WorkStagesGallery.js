75f95ac3f318d5b8d02b79f2073ec86666f3d210
oleg <olegsmushko@gmail.com>
2023-08-30 11:19:17 +0300

    Work stages gallery component

---
+++ src/components/WorkStagesGallery/WorkStagesGallery.js
@@ -0,0 +1,61 @@
+import "./WorkStagesGallery.css"
+import { ArrowHeading } from "../ArrowHeading/ArrowHeading"
+import { MobileSwiper } from "../MobileSwiper/MobileSwiper"
+import { SwiperSlide } from "swiper/react"
+import { PhotoSlider } from "react-photo-view"
+import useDisclosure from "../../hooks/useDisclosure"
+import { useState } from "react"
+
+export const WorkStagesGallery = ({ images }) => {
+	const { isOpen, onClose, onOpen } = useDisclosure()
+	const [index, setIndex] = useState(0)
+
+	const handleOpenGallery = imgIndex => {
+		setIndex(imgIndex)
+		onOpen()
+	}
+
+	const galleryImages = images ?? []
+
+	if (images?.length <= 0) return null
+
+	return (
+		<>
+			<ArrowHeading title="Галерея" />
+			<div className="work-stages-gallery">
+				{images?.map((item, index) => (
+					<img
+						key={`work-stage-${item.created_at}`}
+						alt={`work-stage-${item.created_at}`}
+						src={item.image}
+						onClick={() => handleOpenGallery(index)}
+					/>
+				))}
+			</div>
+
+			<div className="works-stages-gallery-mobile">
+				<MobileSwiper>
+					{images?.map((item, index) => (
+						<SwiperSlide
+							key={`work-stage-mobile-${item.created_at}`}
+							onClick={() => handleOpenGallery(index)}
+						>
+							<img alt={`work-stage-mobile-${item.created_at}`} src={item.image} />
+						</SwiperSlide>
+					))}
+				</MobileSwiper>
+			</div>
+
+			<PhotoSlider
+				images={galleryImages.map(item => ({
+					src: item.image,
+					key: `gallery-slider-${item.created_at}`,
+				}))}
+				visible={isOpen}
+				onClose={onClose}
+				index={index}
+				onIndexChange={setIndex}
+			/>
+		</>
+	)
+}
